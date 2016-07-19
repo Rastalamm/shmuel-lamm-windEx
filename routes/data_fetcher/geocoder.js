db58e4a319f752e04e17cb47ad6e7c5396124dd1
snlamm <shmuel.lamm@gmail.com>
2016-07-19 09:47:38 -0400

    add geocoder and findCoordinates

---
+++ routes/data_fetcher/geocoder.js
@@ -0,0 +1,30 @@
+// use npm geocoder to find coordinates
+var NodeGeocoder = require('node-geocoder');
+// use npm geolib to find coordinate midpoints
+var geoLib = require('geolib')
+// options for geocoder
+var options = {
+  provider: 'google'
+};
+
+var geocoder = NodeGeocoder(options);
+
+// input address. output address and x/y coordinates
+function findCoordinates(address) { geocoder.geocode(address)
+  .then(function(res) {
+    let data = {}
+    data['address'] = res[0].formattedAddress
+    data['latitude'] = res[0].latitude
+    data['longitude'] = res[0].longitude
+    console.log(data);
+    return data
+  })
+  .catch(function(err) {
+    console.log(err);
+    return err
+  });
+}
+
+// place this in the runner and substitute input with form params
+findCoordinates('29 champs elysée paris')
+findCoordinates('350 Fifth Avenue')
